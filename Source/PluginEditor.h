bf23713519c2bdf4815b83daacfd9c8448b9d71d
makislayer <carlosj93v@gmail.com>
2022-03-08 22:03:22 +0100

    Pintado de los sliders personalizados

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -12,22 +12,22 @@
 #include "PluginProcessor.h"
 
 struct LookAndFeel : juce::LookAndFeel_V4 {
-    void drawRotarySlider (juce::Graphics&,
-                            int x, int y, int width, int height,
-                            float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle,
-                            juce::Slider&) override {}
+    void drawRotarySlider(juce::Graphics&,
+        int x, int y, int width, int height,
+        float sliderPosProportional, float rotaryStartAngle, float rotaryEndAngle,
+        juce::Slider&) override;
 };
 
-struct RotarySliderWithLabel : juce::Slider {
-    RotarySliderWithLabel(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+struct RotarySliderWithLabels : juce::Slider {
+    RotarySliderWithLabels(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
         juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag, juce::Slider::TextEntryBoxPosition::NoTextBox), param(&rap), suffix(unitSuffix) 
     {
         setLookAndFeel(&lnf);
     }
-    ~RotarySliderWithLabel() {
+    ~RotarySliderWithLabels() {
         setLookAndFeel(nullptr);
     }
-    void paint(juce::Graphics& g) override {}
+    void paint(juce::Graphics& g) override;
     juce::Rectangle<int> getSliderBounds() const;
     int getTextHeight() const { return 14; }
     juce::String getDisplayString() const;
@@ -73,7 +73,7 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
-    RotarySliderWithLabel peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
+    RotarySliderWithLabels peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider, lowCutSlopeSlider, highCutSlopeSlider;
 
     using APVTS = juce::AudioProcessorValueTreeState;
     using Attachment = APVTS::SliderAttachment;
